13ae9d890a1b933f05a2486e45708d52398c678a
Michael Aydinbas <michael.aydinbas@gmail.com>
2021-09-19 19:40:25 +0200

    update nb

--- notebooks/gov_extraction_full.py
+++ notebooks/gov_extraction_full.py
@@ -6,11 +6,11 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.12.0
+#       jupytext_version: 1.11.5
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3.9 (compgen2)
 #     language: python
-#     name: python3
+#     name: compgen2
 # ---
 
 # %% [markdown]
@@ -26,6 +26,9 @@ import pandas as pd
 import numpy as np
 from compgen2 import GOV, Matcher
 from pathlib import Path
+
+# %% [markdown]
+# ## Using GOV
 
 # %%
 data_root = "../data"
@@ -45,66 +48,6 @@ gov.type_names
 
 # %%
 gov.relations
-
-# %% tags=[] jupyter={"outputs_hidden": true}
-gov.all_paths()
-
-# %% tags=[]
-from dataclasses import dataclass, field
-
-@dataclass(frozen=True)
-class GOVItem:
-    """Class for working with GOV items."""
-    id: int
-    textual_id: str
-    names: set[str] = field(compare=False)
-    types: set[str] = field(compare=False)
-    type_ids: set[int] = field(compare=False, repr=False)
-    children: set[int] = field(compare=False, repr=False)
-    parents: set[int] = field(compare=False, repr=False)
-    paths: set[tuple[int, ...]] = field(compare=False, repr=False)
-
-
-ids = gov.names.id.unique()
-gov_items = {}
-
-for id_ in ids:
-    textual_id = gov.items.query("id == @id_").textual_id.values[0]
-    item_names = set(gov.names.query("id == @id_").content)
-        
-    type_ids = set(gov.types.query("id == @id_").content)
-    item_type_ids = type_ids
-    
-    type_names = set()
-    for type_id in type_ids:
-        type_names.update(gov.type_names.loc[type_id])
-    item_type_names = type_names
-    
-    item_parents = set(gov.relations.query("child == @id_").parent)
-    item_children = set(gov.relations.query("parent == @id_").child)
-    
-    item_paths = set()
-    for path in gov.all_paths():
-        if id_ in path:
-            item_paths.add(path)
-             
-    gov_item = GOVItem(
-        id_,
-        textual_id,
-        item_names,
-        item_type_names,
-        item_type_ids, 
-        item_children,
-        item_parents,
-        item_paths
-    )
-    
-    print(gov_item) 
-    print(gov_item.children)
-    print(gov_item.parents)
-    print(gov_item.paths)
-    
-    
 
 # %%
 paths = gov.all_paths()
@@ -148,10 +91,13 @@ gov.decode_paths_name({gov.get_all_ids_for_name("Krefeld")})
 # %%
 set().update(gov.get_all_ids_for_name("Krefeld"))
 
+# %% [markdown]
+# ## Using Matcher
+
 # %%
 matcher = Matcher(gov)
 
-# %%
+# %% tags=[]
 matcher.find_relevant_paths("Blasdorf, Landeshut")
 
 # %%
@@ -169,10 +115,16 @@ matcher.group_relevant_paths_by_query(matcher.find_relevant_paths("Aach, Freuden
 # %% tags=[] jupyter={"outputs_hidden": true}
 matcher.group_relevant_paths_by_query(matcher.find_relevant_paths("Freudenstadt"), "Freudenstadt")
 
-# %% jupyter={"outputs_hidden": true} tags=[]
+# %%
+gov.all_reachable_nodes_by_id()[1279230]
+
+# %% jupyter={"outputs_hidden": true, "source_hidden": true} tags=[]
 matcher.group_relevant_paths_by_query(matcher.find_relevant_paths("Neustadt, Sachsen"), "Neustadt, Sachsen")
 
 # %% tags=[] jupyter={"outputs_hidden": true}
 gov.decode_paths_name(matcher.find_relevant_paths("Neustadt"))
+
+# %% jupyter={"outputs_hidden": true} tags=[]
+matcher.find_relevant_paths("Sachsen")
 
 # %%
