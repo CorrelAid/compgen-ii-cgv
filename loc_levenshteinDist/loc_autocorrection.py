756cd38178829356e08e7deeb9c1ca4c85c6a999
sitongye <sitongye94@outlook.com>
2021-09-27 18:26:51 +0200

    modify import to be dictionary instead of txt

--- loc_levenshteinDist/loc_autocorrection.py
+++ loc_levenshteinDist/loc_autocorrection.py
@@ -3,18 +3,19 @@ from trie_levenshtein import TrieNode
 class LocCorrection:
     def __init__(self, dictionary):
         """
-        :param dictionary: txt dictionary
+        :param dictionary: dict item, (key, value)-> (id, word)
         """
         self. dictionary = dictionary
         self.WordCount = 0
         self.trie = TrieNode()
         self._dict2trie()
+        self.reverse_dictionary = {v:k for (k,v) in self.dictionary.items()}
 
     def _dict2trie(self):
         # read dictionary file into a trie
-        for word in open(self.dictionary, "rt").read().split():
+        for word in self.dictionary.values():
             self.WordCount += 1
-            self.trie.insert(word)
+            self.trie.insert(word.lower())
         print("Read %d words" % (self.WordCount))
 
         # The search function returns a list of all words that are less than the given
@@ -48,7 +49,7 @@ class LocCorrection:
         # if the last entry in the row indicates the optimal cost is less than the
         # maximum cost, and there is a word in this trie node, then add it.
         if currentRow[-1] <= maxCost and node.word != None:
-            results.append((node.word, currentRow[-1]))
+            results.append((node.word, self.reverse_dictionary[node.word], currentRow[-1]))
         # if any entries in the row are less than the maximum cost, then
         # recursively search each branch of the trie
         if min(currentRow) <= maxCost:
@@ -58,6 +59,6 @@ class LocCorrection:
                     )
 
 # example
-lC = LocCorrection("../data/gov_dictionary.txt")
+lC = LocCorrection({"abc":'aachen'})
 results = lC.search("aaachen", 1)
 print(results)
