9bf245fdb24f3f16e602de3ddaca8504b0fdfc77
Dominik Hohmann <dominikmn@users.noreply.github.com>
2021-10-14 12:56:53 +0200

    Fix tests. Remove test test_get_ids().
    The test test_get_ids() does not make sense in this way.
    Hamburg for example does have 5 different German names in the GOV.

--- compgen2/tests/test_gov_extraction.py
+++ compgen2/tests/test_gov_extraction.py
@@ -8,21 +8,18 @@ def gov(data_root):
     return gov
 
 def test_read_gov_data(gov):
+    gov.load_data()
     assert gov.items.dtypes.to_list() == [np.int32, object, bool]
-    assert gov.names.dtypes.to_list() == [np.int32, object, np.int64, np.int64]
+    assert gov.names.dtypes.to_list() == [np.int32, object, object, np.int64, np.int64]
     assert gov.types.dtypes.to_list() == [np.int32, np.int32, np.int64, np.int64]
     assert gov.relations.dtypes.to_list() == [np.int32, np.int32, np.int64, np.int64]
-    assert gov.type_names.dtypes.to_list() == [object]
+    assert gov.type_names.dtypes.to_list() == [int, object, object]
 
 
 def test_build_paths(gov):
-    paths = gov.all_paths()
+    gov.load_data()
+    gov.build_indices()
+    paths = gov.all_paths
     assert isinstance(paths, set)
     assert all(isinstance(path, tuple) for path in paths)
     assert all(isinstance(id_, int) for path in paths for id_ in path)
-
-
-def test_get_ids(gov):
-    ids = gov.get_all_ids_for_name("Krefeld")
-    names = gov.decode_paths_name({ids})
-    assert all(s == "Krefeld" for s in names)
