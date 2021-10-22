154574059d03b15d67b3283e863560c272337ea2
Dominik Hohmann <dominikmn@users.noreply.github.com>
2021-10-22 11:59:15 +0200

    Issue #43: Adding examples to the notebook.

--- notebooks/issue-43-koelner-phonetik.py
+++ notebooks/issue-43-koelner-phonetik.py
@@ -42,24 +42,64 @@ gov.build_indices()
 ph = Phonetic()
 
 # %%
+from collections import defaultdict
+
+# %%
 phonetic_by_name = {}
+names_by_phonetic = defaultdict(set)
 for n in gov.ids_by_name.keys():
-    phonetic_by_name[n] = ph.encode(n)
+    code = ph.encode(n)
+    phonetic_by_name[n] = code
+    names_by_phonetic[code] |= {n}
+names_by_phonetic.default_factory = None
 
 # %%
 len(gov.ids_by_name.keys()), len(set(phonetic_by_name.values()))
 
-# %%
-from collections import defaultdict
-names_by_phonetic = defaultdict(set)
-for k, v in phonetic_by_name.items():
-    names_by_phonetic[v] |= {k}
-names_by_phonetic.default_factory = None
+# %% [markdown]
+# ## Beispiele: Stärken
+
+# %% [markdown]
+# Die Kölner Phonetik definiert 9 unterschiedliche Laute und vergibt ihnen Codes von 0 bis 8.
+
+# %% [markdown]
+# Sämtliche **Vokale** werden auf "0" abgebildet. Außerdem werden alle "0" im finalen Code gelöscht, bis auf einen potentiellen Vokal am Anfang. Wegen dieser besonderen Behandlung von Vokalen ist die Kölner-Phonetik bei Fehlern in Vokalen besonders invariant.
 
 # %%
-list(gov.ids_by_name.keys())[:10]
+print(ph.encode("Düsseldorf"))
+print(ph.encode("Dassoldurf"))
+print(names_by_phonetic["285273"])
+
+# %% [markdown]
+# Die Kodierung der Kölner-Phonetik reduziert außerdem einen Laut, der mehrmals hintereinander vorkommt auf, auf einen.
 
 # %% tags=[]
-names_by_phonetic[ph.encode("Düsseldorf")]
+print(ph.encode("DDDDDüssssselllllddddorf"))
+
+# %% [markdown]
+# Ähnlichklingende **Konsonanten** haben den selben Laut-Code
 
 # %%
+print(ph.encode("Düßeldorv"))
+
+# %% [markdown]
+# ## Beispiele: Schwächen
+
+# %% [markdown]
+# Was die Kölner-Phonetik **nicht** kann, ist gravierende Fehler in den Laut-Codes zu ignorieren. Der Buchstabe "L" erzeugt einen anderen Laut-Code als "D". Somit hat "Lüsseldorf" in der Kölner-Phonetik einen anderen Code.
+
+# %%
+print(ph.encode("Lüsseldorf"))
+
+# %% [markdown]
+# Insbesondere das **Hinzufügen von Konsonanten** kann die Kölner Phonetik nicht ignorieren.
+
+# %%
+print(ph.encode("Trakischken"))
+print(ph.encode("Trakirschken"))
+
+# %% [markdown]
+# Ebenso das Hinzufügen von ganzen Wörtern verändert den Code.
+
+# %%
+print(ph.encode("Gr. Trakischken, Goldap"))
