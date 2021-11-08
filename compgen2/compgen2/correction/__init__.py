45ad92ceb7398da85b7180b0fdb56708621d1e07
Patrizia Maier <maier_patriziaweb.de>
2021-11-08 20:42:04 +0100

    Change preprocessing classes in __init__

--- compgen2/compgen2/correction/__init__.py
+++ compgen2/compgen2/correction/__init__.py
@@ -1,3 +1,4 @@
 from .loc_autocorrection import LocCorrection
 from .phonetic import Phonetic
-from .preprocessing import Preprocessing
+from .preprocessing_vl import Preprocessing_VL
+from .preprocessing_gov import Preprocessing_GOV
