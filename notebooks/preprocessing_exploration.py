84b0bf6773508ba6303badd9933468227d66c93b
Patrizia Maier <maier_patriziaweb.de>
2021-11-08 21:55:45 +0100

    Update substitution list (work in progress)

--- notebooks/preprocessing_exploration.py
+++ notebooks/preprocessing_exploration.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 import re
-#from compgen2 import Preprocessing_VL
+from compgen2 import Preprocessing_VL
 
 # %% [markdown]
 # # Preprocessing
@@ -93,50 +93,80 @@ vl = verlustliste.copy()
 
 
 # %%
-def prep_clean_brackets(column: pd.Series):
-
+def replace_corrections_vl(column: pd.Series):
+    """Function for removing historical corrections '()[]' and modern-day corrections '{}' and variants
+    1. brackets including their content 
+    2. the word 'nicht' plus related content
+    3. the word 'korrigiert' and its variants plus related content
+    4. the word 'vermutlich' and its variants plus related content
+    """
+        
     # define cases 
-    str_01 =  r'\(.*?\)' # brackets variants incl. content
-    str_02 =  r'\[.*?\]'
-    str_03 =  r'\{.*?\}'
-    str_04 =  r'\(.*?\]'
-    str_05 =  r'\[.*?\)'
-    str_06 =  r'\(.*?\}'
-    str_07 =  r'\{.*?\)'
-    str_08 =  r'\[.*?\}'
-    str_09 =  r'\{.*?\]'
-    str_10 =  r'(\Wnicht.*?)(?=,|$)'
-    str_11 =  r'(\Wkorr.*?|\WKorr\..*?)(?=,|$)'
-    str_12 =  r'(\Wverm.*?)(?=,|$)'
-    
+    str_01 = r'\(.*?\)' # bracket variants incl. content
+    str_02 = r'\[.*?\]'
+    str_03 = r'\{.*?\}'
+    str_04 = r'\(.*?\]'
+    str_05 = r'\[.*?\)'
+    str_06 = r'\(.*?\}'
+    str_07 = r'\{.*?\)'
+    str_08 = r'\[.*?\}'
+    str_09 = r'\{.*?\]'
+    str_10 = r'(\Wnicht.*?)(?=,|$)' # word 'nicht' and following content, until (not including) comma or end of line
+    str_11 = r'(\Wkorr.*?|\WKorr\..*?)(?=,|$)' # word 'korr' and following content, until (not including) comma or end of line
+    str_12 = r'(\Wverm.*?)(?=,|$)'
+
     rep = ''
     
     # replace with remove 
     return column.replace(
         to_replace=[str_01, str_02, str_03, str_04, str_05, str_06, str_07, str_08, str_09, str_10, str_11, str_12], value=rep, regex=True)
     
-vl = prep_clean_brackets(vl)
+vl = replace_corrections_vl(vl)
+
+# %%
+vl
+
+
+# %%
+def replace_characters_vl(column: pd.Series): 
+    """Function for removing special characters: 
+    1. simply removed: ?^_"#*\:{}()[]!
+    2. replaced with ': ´`
+    """
+        
+    # replace with remove 
+    char_1 = '[?^_"#*\:{}()[\]!]'
+    rep_1 = ''
+        
+    # replace with special character (')
+    char_2 = '[´`]'
+    rep_2 = '\''
+        
+    # do replacement 
+    return column.replace(to_replace=[char_1, char_2], value=[rep_1, rep_2], regex=True)
+
+vl = replace_characters_vl(vl)
 
 # %%
 vl
 
 # %%
-# INFO: Zeige Einträge mit Sonderzeichen an 
-char = "Zoszkolon"
+# INFO: Zeige Einträge an 
+char = "Pr.-"
 
 vl[vl.location.str.contains(char)]
 
 
 # %%
-vl = Preprocessing.prep_vl_abbreviations(vl)
+#vl = Preprocessing.prep_vl_abbreviations(vl)
 
 # %%
 # Check: Prozentzahl an weiterhin bestehenden Abkürzungen
-vl.location.str.count("[A-Za-zäöüßÄÖÜẞ]+\.").sum() / vl.shape[0]
+#vl.location.str.count("[A-Za-zäöüßÄÖÜẞ]+\.").sum() / vl.shape[0]
 
 # %%
 # Check: Häufigste weiterhin bestehende Abkürzungen
-vl.location.str.extract("(?P<Abkürzung>[A-Za-zäöüßÄÖÜẞ]+\.)").dropna().value_counts(normalize = True)[0:50]
+#vl.location.str.extract("(?P<Abkürzung>[A-Za-zäöüßÄÖÜẞ]+\.)").dropna().value_counts(normalize = True)[0:50]
 
 # %%
 # INFO: Zeige Einträge mit Sonderzeichen an 
@@ -151,23 +181,31 @@ verlustliste[verlustliste.location.str.contains(char)]
 # INFO: Sonderzeichen . kennzeichnet Abkürzungen 
 
 # %%
+with open('../data/substitutions_vl_gov.csv') as f:
+    print(f)
+
+# %%
 # Lade definierte Abkürzungserweiterungen
-substitutions = pd.read_csv("../data/substitutions_011121.csv", sep = ";", header = None, 
-                            names = ["abbreviation", "expansion"], comment='#')
+substitutions = pd.read_csv("../data/substitutions_vl_gov.csv", sep = ";", header = None, 
+                            names = ["abbreviation", "expansion"], comment='#', encoding='utf-8')
+
+# %%
+# preprocess: regex-compatible and lower case
+substitutions.abbreviation = "((?<=\W)|^)" + substitutions.abbreviation.replace(to_replace='\.', value='\\.', regex=True).str.lower()
+substitutions.abbreviation
+
+# %%
+# abbreviations lower case
+substitutions.expansion = substitutions.expansion.str.lower()
+substitutions.expansion
 
 # %%
 substitutions.info()
-
-# %%
 substitutions
 
 # %%
-# INFO: Abkürzungen ohne Bedeutung werden im Folgenden entfernt
-substitutions[substitutions.expansion == " "]
-
-# %%
-# INFO: Diese Abkürzungen werden durch Komma ersetzt
-substitutions[substitutions.expansion == ", "]
+# INFO: Diese Abkürzungen werden im Folgenden entfernt
+substitutions[substitutions.expansion == "  "]
 
 # %%
 # Substitutions in dict Format
@@ -175,23 +213,23 @@ replace_dict = dict(zip(substitutions.abbreviation, substitutions.expansion))
 replace_dict
 
 # %%
-# INFO: Test Methode mit str.replace() 
-# with_abbreviations = verlustliste[verlustliste.location.str.contains("[A-Za-zäöüßÄÖÜẞ]+\.")].copy()
-# with_abbreviations
+# TEST data set
+with_abbreviations = vl[vl.location.str.contains("[A-Za-zäöüßÄÖÜẞ]+\.")].copy()
+with_abbreviations["location"] = with_abbreviations["location"].str.lower()
+with_abbreviations
+
+# %%
+testset = with_abbreviations.sample(n=20)
+testset
 
 # %%
 # INFO: Test Methode mit replace(regex=False)
 # expanded = with_abbreviations.location.replace(replace_dict, regex=False)
 # expanded
 
-# Problem: Matched keine Substrings -> nicht geeignet
+# Problem: Matched keine Substrings -> grundsätzlich nicht geeignet
 
-# %%
-# INFO: Test Methode mit replace(regex=True)
-# expanded_re = with_abbreviations.location.replace(replace_dict, regex=True)
-# expanded_re
-
-# Problem: Falsche Matches, da . in Dict als regex interpretiert wird -> nicht geeignet
+# column.replace(to_replace=subst_dict, regex=False)
 
 # %%
 # INFO: Test Methode mit str.replace, benötigt loop  
@@ -199,61 +237,38 @@ replace_dict
 #    with_abbreviations['location'] = with_abbreviations['location'].str.replace(old, new, regex=False)
 # with_abbreviations
 
-# Das funktioniert! 
+# Funktioniert! Matched Substrings aber ist ineffizient
+
+# %%
+# INFO: Test Methode mit replace(regex=True)
+testset.location = testset.location.replace(replace_dict, regex=True)
+testset
+
+# Funktioniert! 
+
+# %%
+with_abbreviations.location.str.count("[A-Za-zäöüßÄÖÜẞ]+\.").sum() / with_abbreviations.shape[0]
+
+# %%
+with_abbreviations.location.str.extract("(?P<Abkürzung>[A-Za-zäöüßÄÖÜẞ]+\.)").dropna().value_counts(normalize = True)[0:50]
+
+# %%
+# INFO: Zeige Einträge an 
+char = "kl\."
+
+with_abbreviations[with_abbreviations.location.str.contains(char)]
 
 # %%
 # Ersetze Abkürzungen in Verlustliste
-for old, new in replace_dict.items():
-    verlustliste['location'] = verlustliste['location'].str.replace(old, new, regex=False)
-verlustliste
+# vl.location = vl.location.replace(replace_dict, regex=True)
+# vl
 
 # %%
 # Check: Prozentzahl an weiterhin bestehenden Abkürzungen
-verlustliste.location.str.count("[A-Za-zäöüßÄÖÜẞ]+\.").sum() / verlustliste.shape[0]
+# vl.location.str.count("[A-Za-zäöüßÄÖÜẞ]+\.").sum() / vl.shape[0]
 
 # %%
 # Check: Häufigste weiterhin bestehende Abkürzungen
-verlustliste.location.str.extract("(?P<Abkürzung>[A-Za-zäöüßÄÖÜẞ]+\.)").dropna().value_counts(normalize = True)[0:50]
-
-# %% [markdown]
-# ### Save as parquet
-
-# %%
-verlustliste.to_parquet("../data/deutsche-verlustlisten-1wk_preprocessed.parquet")
-
-# %% [markdown]
-# ## GOV
-
-# %%
-gov = pd.read_parquet("../data/gov_orte_v01.parquet")
-
-# %%
-gov.info()
-
-# %%
-gov
-
-# %%
-# ´ ` und ' vereinheitlichen auf ' 
-gov = gov.replace({'location' : { '´' : '\'', '`' : '\''}}, regex=True)
-
-# %%
-# Einträgt mit ? ! & + _ * > { } komplett entfernen
-chars = ['?', '!', '&', '+', '_', '*', '>', '{', '}', ':']
-
-gov = gov[~gov.location.apply(lambda loc: any(c in loc for c in chars))]
-gov
-
-# %%
-# TBD: Abkürzungen mit . erweitern?
-
-# %%
-# TBD: Übersetzungen mit = getrennt aufsplitten?
-
-# %% [markdown]
-# ### Save as parquet
-
-# %%
-gov.to_parquet("../data/gov_orte_v01_preprocessed.parquet")
+# vl.location.str.extract("(?P<Abkürzung>[A-Za-zäöüßÄÖÜẞ]+\.)").dropna().value_counts(normalize = True)[0:50]
 
 # %%
