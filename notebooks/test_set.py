74ab8e6726b3d59b482b764fb1324f1fbabbb4ea
Michael Aydinbas <michael.aydinbas@gmail.com>
2021-11-15 20:58:34 +0100

    test samples of vl

--- notebooks/test_set.py
+++ notebooks/test_set.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # +
 import pickle
+from pathlib import Path
 
 import pandas as pd
 
 from compgen2 import GOV, Matcher, GovTestData, Preprocessing_VL
+from compgen2.const import VL_FILE
 
 # +
 from pathlib import Path
@@ -18,7 +20,7 @@ Path(".").cwd()
 # %load_ext autoreload
 # %autoreload 2
 
-data_root= "..data/"
+data_root= "../data/"
 
 gov = GOV(data_root)
 gov.load_data()
@@ -61,5 +63,58 @@ m = Matcher(gov)
 m.get_match_for_locations(valid_test_locations)
 
 gtd.get_accuracy(m.results)
+
+
+# ## Original VL ohne Preprocessing
+
+def read_vl(data_root) -> pd.DataFrame:
+    path = Path(data_root) / VL_FILE
+    parquet_filename = path.parent / (
+        path.stem + ".parquet"
+    )  # replace .csv with .parquet
+
+    def get_location_parts_count(loc):
+        return len(loc.split(","))
+
+    # read in tsv and save as parquet, if not already done
+    if not Path(parquet_filename).exists():
+        vl = pd.read_csv(
+            path, sep="\t", header=None, names=["loc_count", "location"]
+        )
+        vl = vl[~vl.location.isna()]
+        vl = vl.assign(loc_parts_count=vl.location.map(get_location_parts_count))
+        vl.to_parquet(parquet_filename)
+
+    return pd.read_parquet(parquet_filename)
+
+
+vl = read_vl(data_root)
+
+vl.sample(10)
+
+
+
+performance = []
+N = 100
+for i in range(10):
+    valid_test_locations = vl.location.sample(N)
+    valid_test_locations_preprocessed = Preprocessing_VL.replace_corrections_vl(valid_test_locations)
+    valid_test_locations_preprocessed = Preprocessing_VL.replace_characters_vl(valid_test_locations_preprocessed)
+    
+    m_one = Matcher(gov)
+    m_one.get_match_for_locations(valid_test_locations)
+    
+    m_two = Matcher(gov)
+    m_two.get_match_for_locations(valid_test_locations_preprocessed)
+    
+    performance.append(
+        (
+            len([entry for entry in m_one.results.values() if entry["possible_matches"]]) / N, 
+            len([entry for entry in m_two.results.values() if entry["possible_matches"]]) / N
+        )
+    )
+    
+
+performance
 
 
